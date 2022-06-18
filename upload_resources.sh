e22aaddf15315e3273927d748cc0ea3762d837d6
João Mateus de Freitas Veneroso <jmfveneroso@gmail.com>
2022-06-18 15:13:32 -0300

    Tidy up.

--- upload_resources.sh
+++ upload_resources.sh
@@ -7,6 +7,3 @@ echo "Creating tarball..."
 now=$(date +'%Y-%m-%d')
 filename=wizard-resources-$now.tar.gz
 tar -czf /tmp/$filename resources
-
-# s3cmd put /tmp/$filename s3://wizard-resources/$filename
-# s3cmd ls s3://wizard-resources/
